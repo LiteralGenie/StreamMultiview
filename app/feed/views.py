0f94faa06e5d15f3051f6755a1c9af01ccf44acb
Genie <frothsavd@gmail.com>
2021-04-09 19:44:17 -0400

    img with time suffix

--- app/feed/views.py
+++ app/feed/views.py
@@ -1,13 +1,11 @@
 from flask import views, Response
-from utils.stream_utils import get_feed_gen
+from utils.stream_utils import read_feed
 
 
 class FeedView(views.MethodView):
 	def __init__(self, url):
 		super().__init__()
 		self.url= url
-		self.sep= 'sep'
 
 	def get(self):
-		return Response(get_feed_gen(self.url, sep=self.sep),
-						mimetype=f'multipart/x-mixed-replace; boundary={self.sep}')
+		return read_feed(self.url)
--- utils/stream_utils.py
+++ utils/stream_utils.py
@@ -1,18 +1,60 @@
-import cv2, re
+from flask import send_file
+import cv2, re, time, io, threading
 
 
-def get_feed_gen(url, encode=True, **kwargs):
-	cap= cv2.VideoCapture(url)
-	while cap.isOpened():
-		ret,frame= cap.read()
-		if ret:
-			if encode:
-				frame= web_encode(frame, **kwargs)
-			yield frame
+class Feed:
+	def __init__(self, url):
+		self.url= url
+		self.cap= cv2.VideoCapture(url)
+		self.last_frame= self.cap.read()[1]
 
-def web_encode(image, sep="sep", extension="jpg", mimetype="image/jpeg"):
+		threading.Thread(target=self.start, args=()).start()
+
+	def start(self):
+		while True:
+			ret, frame= self.cap.read()
+			if ret:
+				self.last_frame= frame
+
+
+feed_dict= {}
+
+def get_feed(url):
+	global feed_dict
+
+	if url not in feed_dict:
+		feed_dict[url]= Feed(url)
+	return feed_dict[url]
+
+# def get_feed_gen(url, encode=True, **kwargs):
+# 	cap= get_feed(url)
+# 	while cap.isOpened():
+# 		ret,frame= cap.read()
+# 		if ret:
+# 			if encode:
+# 				frame= web_encode(frame, **kwargs)
+#
+# 			yield frame
+# 			return
+
+# def web_encode(image, sep="sep", extension="png", mimetype="image/png"):
+# 	extension= re.sub(r'^\.*', '', extension)
+# 	_,image= cv2.imencode(f'.{extension}', image)
+# 	return b'--' + sep.encode() + \
+# 		   b'\r\nContent-Type: ' + mimetype.strip().encode() + b'\r\n\r\n' + \
+# 		   bytes(image) + b'\r\n'
+
+def web_encode(image, extension="png", mimetype="image/png"):
 	extension= re.sub(r'^\.*', '', extension)
 	_,image= cv2.imencode(f'.{extension}', image)
-	return b'--' + sep.encode() + \
-		   b'\r\nContent-Type: ' + mimetype.strip().encode() + b'\r\n\r\n' + \
-		   bytes(image) + b'\r\n'
+
+	image= io.BytesIO(image)
+
+	return send_file(image, mimetype=mimetype, cache_timeout=-1)
+
+def read_feed(url):
+	feed= get_feed(url)
+	frame= feed.last_frame
+	frame= cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+	frame= web_encode(frame)
+	return frame
