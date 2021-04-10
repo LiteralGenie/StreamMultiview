df143f34f3fdd19d953abc0aa00bb0ff6c414f35
Literal Genie <frothsavd@gmail.com>
2021-04-10 11:59:25 -0700

    threads

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from app.app import app
 from waitress import serve
 
 if __name__ == "__main__":
-	serve(app, host='0.0.0.0', port=8282, threads=4)
+	serve(app, host='0.0.0.0', port=8282, threads=40)
